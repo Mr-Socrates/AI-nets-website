cb17bb85df7ef818fbda016ed2708ec265571871
Papa Socrates <silhavydan@gmail.com>
2018-12-02 17:52:27 +0200

    HA! Login functioning with Firebase

--- js/login.js
+++ js/login.js
@@ -17,6 +17,7 @@ function CollectData() {
 }
 
 function LogIn() {
+    // TODO: Validation function
     const email = $("#inputEmail").val();
     const password = $("#inputPassword").val();
     alert("Given email: " + email + "\nGiven password: " + password);
@@ -49,44 +50,8 @@ function LogIn() {
     });
 }
 
-// linkSignUp.addEventListener('click', e =>  {
-//
-//     // TODO: Validation
-//
-//     alert("Given email: " + email + "\nGiven password: " + password);
-//
-//     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         alert("Create: User not created. Code: " + errorCode + "\nError Message: " + errorMessage);
-//     });
-//
-//     firebase.auth().onAuthStateChanged(function(user) {
-//         alert("AUTH State changed on: " + user.email);
-//         if (user) {
-//             // User is signed in.
-//             const displayName = user.displayName;
-//             const email = user.email;
-//             const emailVerified = user.emailVerified;
-//             const photoURL = user.photoURL;
-//             const isAnonymous = user.isAnonymous;
-//             const uid = user.uid;
-//             const providerData = user.providerData;
-//
-//             alert("Signed In. Data Collected:\n" + displayName + "\n"+email + "\n"+emailVerified + "\n"+photoURL + "\n"+isAnonymous + "\n"+uid + "\n"+providerData);
-//             // ...
-//         } else {
-//             // User is signed out.
-//             // ...
-//             alert("Signed OUT. Data Collected:\n" + displayName + "\n"+email + "\n"+emailVerified + "\n"+photoURL + "\n"+isAnonymous + "\n"+uid + "\n"+providerData);
-//         }
-//     });
-// });
-
 
 function Create() {
-    // TODO: Validation
     const email = $("#inputEmail").val();
     const password = $("#inputPassword").val();
     alert("Given email: " + email + "\nGiven password: " + password);
