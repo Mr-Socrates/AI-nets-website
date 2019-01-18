ef2fdb705e59375fbf21ad5b7567445961e4ec6c
inezeit <inez.eit@gmail.com>
2019-01-18 17:01:58 +0200

    send email php into js file

--- mail/registration_email.php
+++ mail/registration_email.php
@@ -1,30 +1,49 @@
-require_once('phpmailer/class.phpmailer.php');
 <?php
-/**
- * Created by PhpStorm.
- * User: Inez
- * Date: 15/01/2019
- * Time: 16:49
- */
+require_once('phpmailer/class.phpmailer.php');
 
-if(isset($_POST['email'])){
-    $email = $_POST['email'];
-}else{
-    return;
+header('Content-Type: application/json');
+
+$aResult = array();
+
+if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
+
+if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
+
+if( !isset($aResult['error']) ) {
+
+    switch($_POST['functionname']) {
+        case 'send':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                $aResult['error'] = 'Error in arguments!';
+            }
+            else {
+
+                $message = "Your account is registered";
+                $to=$email;
+                $subject="Registration";
+                $from = 'our team';
+                $body='hello, your account is activated';
+                $headers = "From:".$from;
+                if (mail ($to, $subject, $body, $headers)) {
+                    echo '<p>Your message has been sent!</p>';
+                } else {
+                    echo '<p>Something went wrong, go back and try again!</p>';
+                }
+
+                echo "An Activation Code Is Sent To You Check You Emails";
+
+
+                $aResult['result'] = send(floatval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
+            }
+            break;
+
+        default:
+            $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+            break;
+    }
+
 }
 
-$message = "Your account is registered";
-$to=$email;
-$subject="Registration";
-$from = 'our team';
-$body='hello, your account is activated';
-$headers = "From:".$from;
-if (mail ($to, $subject, $body, $headers)) {
-    echo '<p>Your message has been sent!</p>';
-} else {
-    echo '<p>Something went wrong, go back and try again!</p>';
-}
-
-echo "An Activation Code Is Sent To You Check You Emails";
+echo json_encode($aResult);
 
 ?>
